a724a331689adf9447cc0a7a53f5bda11fc002ac
Sandra Leoni <hippietop@Sandras-MacBook-Air.local>
2024-07-17 10:22:39 +0300

    Add cancellation

--- Extensions/Sources/Extensions/URLSession+DataTask.swift
+++ Extensions/Sources/Extensions/URLSession+DataTask.swift
@@ -3,14 +3,47 @@ import Foundation
 public extension URLSession {
 
     func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
-        return try await withCheckedThrowingContinuation { continuation in
-            dataTask(with: urlRequest) { data, response, error in
-                if let data, let response {
-                    continuation.resume(with: .success((data, response)))
-                } else if let error {
-                    continuation.resume(throwing: error)
+        let dataTaskHolder = CancellableHolder<URLSessionDataTask>()
+        return try await withTaskCancellationHandler(operation: {
+            try Task.checkCancellation()
+            return try await withCheckedThrowingContinuation { continuation in
+                dataTaskHolder.value = self.dataTask(with: urlRequest) { data, response, error in
+                    guard let data = data, let response = response else {
+                        let error = error ?? URLError(.badServerResponse)
+                        return continuation.resume(throwing: error)
+                    }
+                    continuation.resume(returning: (data, response))
                 }
-            }.resume()
-        }
+                dataTaskHolder.value?.resume()
+            }
+        }, onCancel: {
+            dataTaskHolder.cancel()
+        })
     }
 }
+
+private final class CancellableHolder<T: Cancellable>: @unchecked Sendable {
+    private var lock = NSRecursiveLock()
+    private var innerCancellable: T?
+
+    private func synced<Result>(_ action: () throws -> Result) rethrows -> Result {
+        lock.lock()
+        defer { lock.unlock() }
+        return try action()
+    }
+
+    var value: T? {
+        get { synced { innerCancellable } }
+        set { synced { innerCancellable = newValue } }
+    }
+
+    func cancel() {
+        synced { innerCancellable?.cancel() }
+    }
+}
+
+private protocol Cancellable {
+    func cancel()
+}
+
+extension URLSessionDataTask: Cancellable {}
